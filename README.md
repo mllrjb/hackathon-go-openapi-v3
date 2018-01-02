1ca1f5b3eb0877c7e844b39cfaf163e3f2162e94
Jason Miller <jason.miller@logrhythm.com>
2018-01-02 10:44:12 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+# Go OpenAPI 3.0.0 Generator
+
+Generates a RESTful API based on an OpenAPI 3.0.0 specification.
+
+## Getting Started
+
+```
+go get github.com/googleapis/gnostic
+go run parse.go
+```
---
+++ parse.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/googleapis/gnostic/OpenAPIv3"
+	"github.com/googleapis/gnostic/compiler"
+)
+
+func main() {
+	filepath := "examples/CaseAPI/caseapi.yaml"
+	bytes, err := compiler.ReadBytesForFile(filepath)
+	if err != nil {
+		fmt.Printf("unable to read bytes from %s\n", filepath, err)
+		os.Exit(1)
+	}
+	info, err := compiler.ReadInfoFromBytes(filepath, bytes)
+	if err != nil {
+		fmt.Printf("unable to read info from %s\n", filepath, err)
+		os.Exit(1)
+	}
+
+	document, err := openapi_v3.NewDocument(info, compiler.NewContext("$root", nil))
+	if err != nil {
+		fmt.Printf("unable to parse document %s\n", filepath, err)
+		os.Exit(1)
+	}
+
+	data, err := json.Marshal(document)
+	if err != nil {
+		fmt.Printf("unable to marshal document to JSON %s\n", filepath, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", data)
+	os.Exit(0)
+}
