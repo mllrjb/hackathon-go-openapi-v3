a076ab99929fab6ec392bd26ab2f7a2988039ea0
Anna Podoplelova <Anna.Podoplelova@logrhythm.com>
2018-01-02 16:01:27 -0700

    Generate a controller (#2)
    * Template something
    
    * Correct file name
    
    * Controller template
    
    * git ignore

---
+++ generate.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/googleapis/gnostic/OpenAPIv3"
+)
+
+const templateDir = "./templates"
+
+func generateFiles(document *openapi_v3.Document) {
+	files, err := ioutil.ReadDir(templateDir)
+	if err != nil {
+		fmt.Printf("unable to read template directory: %v", err)
+		os.Exit(1)
+	}
+
+	var templateFiles []string
+	for _, file := range files {
+		filename := file.Name()
+		if strings.HasSuffix(filename, ".tmpl") {
+			templateFiles = append(templateFiles, fmt.Sprintf("%v/%v", templateDir, filename))
+		}
+		//TODO: move .go extension files directly to generated
+	}
+
+	funcMap := template.FuncMap{
+		"Title": strings.Title,
+	}
+
+	t := template.New("template").Funcs(funcMap)
+	t, err = t.ParseFiles(templateFiles...)
+	if err != nil {
+		fmt.Printf("unable to parse template files: %v", err)
+		os.Exit(1)
+	}
+
+	paths := document.GetPaths()
+	if paths != nil {
+		t.ExecuteTemplate(os.Stdout, "controller.tmpl", *paths)
+	}
+
+}
--- parse.go
+++ parse.go
@@ -13,26 +13,29 @@ func main() {
 	filepath := "examples/CaseAPI/caseapi.yaml"
 	bytes, err := compiler.ReadBytesForFile(filepath)
 	if err != nil {
-		fmt.Printf("unable to read bytes from %s\n", filepath, err)
+		fmt.Printf("unable to read bytes from %s %s\n", filepath, err)
 		os.Exit(1)
 	}
 	info, err := compiler.ReadInfoFromBytes(filepath, bytes)
 	if err != nil {
-		fmt.Printf("unable to read info from %s\n", filepath, err)
+		fmt.Printf("unable to read info from %s %s\n", filepath, err)
 		os.Exit(1)
 	}
 
 	document, err := openapi_v3.NewDocument(info, compiler.NewContext("$root", nil))
 	if err != nil {
-		fmt.Printf("unable to parse document %s\n", filepath, err)
+		fmt.Printf("unable to parse document %s %s\n", filepath, err)
 		os.Exit(1)
 	}
 
+	generateFiles(document)
+
 	data, err := json.Marshal(document)
 	if err != nil {
-		fmt.Printf("unable to marshal document to JSON %s\n", filepath, err)
+		fmt.Printf("unable to marshal document to JSON %s %s\n", filepath, err)
 		os.Exit(1)
 	}
 	fmt.Printf("%s\n", data)
+
 	os.Exit(0)
 }
